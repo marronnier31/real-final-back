c53cdf5db9a854f39aaccde32274ce734437ba80
y9828 <y9828@naver.com>
2026-03-21 21:20:38 +0900

    [생성] Controller파일 ReviewController 생성

--- src/main/java/com/kh/trip/service/ReviewService.java
+++ src/main/java/com/kh/trip/service/ReviewService.java
@@ -6,6 +6,6 @@ import com.kh.trip.dto.ReviewSummaryDTO;
 public interface ReviewService {
 
 	// 로그인한 사용자 번호와 리뷰 작성 DTO를 받아서 리뷰를 등록
-	ReviewSummaryDTO createReview(Long loginUserNo, ReviewCreateDTO reviewCreateDTO);
+	public ReviewSummaryDTO createReview(Long loginUserNo, ReviewCreateDTO reviewCreateDTO);
 
 }

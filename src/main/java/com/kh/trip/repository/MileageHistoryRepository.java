ffc89ba3a4efff86905b0775c5ebd1f32b53fdbe
hy3oning <shyun5635@gmail.com>
2026-03-25 11:26:51 +0900

    [update] 중복적립 방지 메서드

--- src/main/java/com/kh/trip/repository/MileageHistoryRepository.java
+++ src/main/java/com/kh/trip/repository/MileageHistoryRepository.java
@@ -5,8 +5,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kh.trip.domain.MileageHistory;
+import com.kh.trip.domain.enums.MileageChangeType;
+import com.kh.trip.domain.enums.MileageStatus;
 
 public interface MileageHistoryRepository extends JpaRepository<MileageHistory, Long> {
 
 	List<MileageHistory> findByPayment_PaymentNo(Long paymentNo);
+
+	boolean existsByPayment_PaymentNoAndChangeTypeAndStatus(Long paymentNo, MileageChangeType changeType,
+			MileageStatus status);
 }

7bf12e7a46c9880c035e9ee47825d2d949242d9c
hy3oning <shyun5635@gmail.com>
2026-03-26 11:12:57 +0900

    [update] 호스트 프로필 승인상태 반려상태 컨트롤러 생성.

--- src/main/java/com/kh/trip/controller/HostProfileController.java
+++ src/main/java/com/kh/trip/controller/HostProfileController.java
@@ -1,8 +1,11 @@
 package com.kh.trip.controller;
 
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -10,10 +13,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.kh.trip.dto.HostProfileDTO;
+import com.kh.trip.security.AuthUserPrincipal;
 import com.kh.trip.service.HostProfileService;
 
 import lombok.RequiredArgsConstructor;
-
 
 @RestController
 @RequiredArgsConstructor
@@ -26,15 +29,27 @@ public class HostProfileController {
 	public Long register(@RequestBody HostProfileDTO hostProfileDTO) {
 		return hostProfileService.register(hostProfileDTO);
 	}
-	
+
 	@GetMapping
-	public List<HostProfileDTO> getList(){
+	public List<HostProfileDTO> getList() {
 		return hostProfileService.getList();
 	}
-	
+
 	@GetMapping("/{hostNo}")
 	public HostProfileDTO get(@PathVariable Long hostNo) {
 		return hostProfileService.get(hostNo);
 	}
-	
+
+	@PatchMapping("/{hostNo}/approve")
+	public Map<String, String> approve(@PathVariable Long hostNo, @AuthenticationPrincipal AuthUserPrincipal authUser) {
+		hostProfileService.approve(hostNo, authUser.getUserNo());
+		return Map.of("result", "SUCCESS");
+	}
+
+	@PatchMapping("/{hostNo}/reject")
+	public Map<String, String> reject(@PathVariable Long hostNo, @RequestBody HostProfileDTO hostProfileDTO,
+			@AuthenticationPrincipal AuthUserPrincipal authUser) {
+		hostProfileService.reject(hostNo, authUser.getUserNo(), hostProfileDTO.getRejectReason());
+		return Map.of("result", "SUCCESS");
+	}
 }

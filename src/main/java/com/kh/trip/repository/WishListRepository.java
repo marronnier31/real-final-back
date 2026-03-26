4e1ab41737a67a76117ba2758e34f8d2d1d023d0
mudi <freedom2424@nate.com>
2026-03-26 18:59:32 +0900

    [update] WishListRepository에 마이페이지 위시리스트 조회 쿼리 추가

--- src/main/java/com/kh/trip/repository/WishListRepository.java
+++ src/main/java/com/kh/trip/repository/WishListRepository.java
@@ -2,7 +2,10 @@ package com.kh.trip.repository;
 
 import java.util.Optional;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kh.trip.domain.Lodging;
 import com.kh.trip.domain.User;
@@ -11,5 +14,8 @@ import com.kh.trip.domain.WishList;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
 	Optional<WishList> findByUserAndLodging(User user, Lodging lodging);
-	
+
+	@EntityGraph(attributePaths = { "lodging", "lodging.imageList" })
+	@Query("select w from WishList w where w.user.userNo = :userNo order by w.regDate desc")
+	java.util.List<WishList> findMypageWishlist(@Param("userNo") Long userNo);
 }

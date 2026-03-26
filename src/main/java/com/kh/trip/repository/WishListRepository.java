63b8d8811ec693e4f24894d84a5d59394f11cb4d
choesoyeong87-ui <choesoyeong87@gmail.com>
2026-03-26 14:27:13 +0900

    [추가]wishList 쿼리문추가

--- src/main/java/com/kh/trip/repository/WishListRepository.java
+++ src/main/java/com/kh/trip/repository/WishListRepository.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import com.kh.trip.domain.Lodging;
 import com.kh.trip.domain.User;
@@ -13,7 +15,7 @@ import com.kh.trip.domain.WishList;
 
 public interface WishListRepository extends JpaRepository<WishList, Long> {
 	Optional<WishList> findByUserAndLodging(User user, Lodging lodging);
-
-	Page<WishList> findByUserId(Long userNo, Pageable pageable);
+	@Query("select w from WishList w where w.user.userNo = :userNo")
+	Page<WishList> findByUserId(@Param("userNo")Long userNo, Pageable pageable);
 	
 }

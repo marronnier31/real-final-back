186444c7fb848971786bd6c28e911c55e76f458b
hy3oning <shyun5635@gmail.com>
2026-03-29 03:01:37 +0900

    [update] toString 제거

--- src/main/java/com/kh/trip/domain/Coupon.java
+++ src/main/java/com/kh/trip/domain/Coupon.java
@@ -26,12 +26,10 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.ToString;
 
 @Entity
 @Table(name = "COUPONS")
 @Getter
-@ToString
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @AllArgsConstructor
 @Builder

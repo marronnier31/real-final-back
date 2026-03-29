80633ff4c75d0d554f77aa1bef2de2b80e9c191b
marronnier31 <marronnier31@gmail.com>
2026-03-29 20:41:56 +0900

    [update] 채팅방목록조회할 때 로그인된 사용자가 호스트인지, 그렇다면 그사람의 호스트 번호는 몇번인지 조회하기 위한 메서드 추가

--- src/main/java/com/kh/trip/repository/HostProfileRepository.java
+++ src/main/java/com/kh/trip/repository/HostProfileRepository.java
@@ -1,5 +1,7 @@
 package com.kh.trip.repository;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.kh.trip.domain.HostProfile;
@@ -9,4 +11,6 @@ public interface HostProfileRepository extends JpaRepository<HostProfile, Long> 
 	boolean existsByUser_UserNo(Long userNo);
 	
 	boolean existsByBusinessNumber(String businessNumber);
+
+	Optional<HostProfile> findByUser_UserNo(Long userNo);
 }
